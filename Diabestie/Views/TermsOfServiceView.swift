014c10a5df80f57d219b408caa4b64caa93a124f
Anika Morris <anikamorris11@gmail.com>
2021-01-07 17:15:38 -0600

    added text of terms and conditions

---
+++ Diabestie/Views/TermsOfServiceView.swift
@@ -0,0 +1,15 @@
+//
+//  TermsOfServiceView.swift
+//  Diabestie
+//
+//  Created by Anika Morris on 1/7/21.
+//  Copyright © 2021 Anika Morris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class TermsAndConditionsView: UIView {
+    
+}
